e2a9da96df88fb69489590efbb56a5db0338c2e2
Alexander Bastidas <xanderjorge@hotmail.com>
2023-10-03 17:48:14 -0500

    Mejora del Codigo y Arreglo de errores

--- Tupla_Ejercicio.py
+++ Tupla_Ejercicio.py
@@ -8,56 +8,51 @@ def productoEscalar(v1,v2):
         producto += v1[i]*v2[i]
     return f"El producto escalar de {v1},{v2} es: {producto}"
 
-def ModificarVector1(v1):
-    elemento_a_Modificar = int(input(f" Ingrese el elemento que desea reemplazar: {v1}"))
-    v1 = list(v1)
-    if elemento_a_Modificar in v1:
-        for i in range(len(v1)):
-            if v1[i] == elemento_a_Modificar:
-                v1[i] = int(input("Ingrese el nuevo elemento: "))
-    print(v1)
-    return tuple(v1)
+def ModificarVector1(vector1):
+    elemento_a_Modificar = int(input(f"Ingrese el elemento que desea reemplazar {vector1}: "))
+    vector1 = list(vector1)
+    if elemento_a_Modificar in vector1:
+        vector1[vector1.index(elemento_a_Modificar)] = int(input("Ingrese el nuevo elemento: "))
+    return tuple(vector1)
 
-
-def ModificarVector2(v2):
-    elemento_a_Modificar = int(input(f" Ingrese el elemento que desea reemplazar: {v2}"))
-    v2 = list(v2)
-    if elemento_a_Modificar in v2:
-        for i in range(len(v2)):
-            if v2[i] == elemento_a_Modificar:
-                v2[i] = int(input("Ingrese el nuevo elemento: "))
-    print(v2)
-    return tuple(v2)
-
+def ModificarVector2(vector2):
+    elemento_a_Modificar = int(input(f" Ingrese el elemento que desea reemplazar {vector2}: "))
+    vector2 = list(vector2)
+    if elemento_a_Modificar in vector2:
+        vector2[vector2.index(elemento_a_Modificar)] = int(input("Ingrese el nuevo elemento: "))
+    return tuple(vector2)
 
 def Modificacion(v1,v2):
     print(menu2())
-    valor = int(input("Ingrese a que vectro va a modificar: "))
-    if(valor == 1):
-        productoEscalar(ModificarVector1(v1),v2)
-    elif(valor == 2):
-        productoEscalar(v1,ModificarVector2(v2))
+    valor = int(input("Ingrese a qué vector va a modificar: "))
+    if valor == 1:
+        newVector1 = ModificarVector1(v1)
+        print(productoEscalar(newVector1, v2))
+    elif valor == 2:
+        newVector2 = ModificarVector2(v2)
+        print(productoEscalar(v1, newVector2))
     else:
-        print("Opcion invalida vuelva a intentar")
-
+        print("Opción inválida. Vuelva a intentar.")
 
 def main():
-    vector1 = (1,2,3)
-    vector2 = (-1,0,2)
-    print("Menu",end="\n\n")
+    vector1 = (1, 2, 3)
+    vector2 = (-1, 0, 2)
+    print("Menu", end="\n\n")
     print(f"1.- Mostrar el producto escalar de los vectores {vector1},{vector2}")
     print(f"2.- Modificar el valor de un vector y mostrar el producto escalar")
     print("3.- Salir")
 
-    match int(input("Elija una opcion: ")):
-        case 1: 
-            print(productoEscalar(vector1,vector2)) 
-            return True
-        case 2: 
-            print(Modificacion(vector1,vector2))
-            return True 
-        case 3: 
-            return False
+    opcion = int(input("Elija una opción: "))
+    if opcion == 1:
+        print(productoEscalar(vector1, vector2))
+    elif opcion == 2:
+        Modificacion(vector1, vector2)
+    elif opcion == 3:
+        return False
+    else:
+        print("Opción inválida. Vuelva a intentar.")
+    
+    return True
 
 if __name__ == '__main__':
     value = True
